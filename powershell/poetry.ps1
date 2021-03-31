ae2c4c0141ecea8450ff000a0e80005c3eb303d8
da-moon <damoon.azarpazhooh@live.com>
2021-03-31 13:46:38 -0400

    Update and rename poetry to poetry.ps1

--- powershell/poetry.ps1
+++ powershell/poetry.ps1
@@ -1,2 +1,2 @@
 (Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python -
-python -m pip install cleo tomlkit poetry.core cachecontrol cachy html5lib pkginfo virtualenv
+python -m pip install cleo tomlkit poetry.core cachecontrol cachy html5lib pkginfo virtualenv lockfile
